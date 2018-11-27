a71c22029dabddc4f3ad3eb6b7945fce281488ad
wiai <wiai@github.com>
2018-11-27 18:10:06 +0100

    apidoc without --implicit-namespaces

--- doc/make_apidoc.bat
+++ doc/make_apidoc.bat
@@ -2,5 +2,5 @@
 :: http://www.sphinx-doc.org/en/1.5.1/invocation.html#invocation-apidoc
 
 RMDIR "api" /S /Q
-sphinx-apidoc -fMe --implicit-namespaces -o api ../src/aloe
+sphinx-apidoc -fMe -o api ../src/aloe
 DEL "api\modules.rst"
